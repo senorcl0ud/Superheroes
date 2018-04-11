f5a1417248edaa50e028c6a078c998d1520024eb
senorcl0ud <claudemcalpin@gmail.com>
2018-04-10 22:06:22 -0400

    added arraylist and iterator

--- src/main/java/superheros/Main.java
+++ src/main/java/superheros/Main.java
@@ -1,5 +1,8 @@
 package superheros;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,17 +15,42 @@ public class Main {
     Superman superman = new Superman("Clark Kent", new String[]{"super strength","super everything","laser eyes"});
     GreenLantern greenLantern = new GreenLantern("John Stewart", new String[]{"lantern ring", "willpower", "greeen light"});
 
-        Superhero[] pantheon = new Superhero[]{ nightcrawler, spawn, spiderman, theFlash, superman};
-        for (Superhero hero : pantheon){
-            System.out.println(hero.listPowers());
-        }
-        System.out.println( spawn.myBlackHero() + " and " + spawn.flightSpeed("9999 mph"));
-        System.out.println( superman.flightSpeed("super fast"));
+        ArrayList<Superhero> theLeague = new ArrayList<>();
+        theLeague.add(nightcrawler);
+        theLeague.add(spawn);
+        theLeague.add(spiderman);
+        theLeague.add(superman);
+        theLeague.add(greenLantern);
+        theLeague.add(theFlash);
 
-        IFlightSpeed[] flyingHeroes = new IFlightSpeed[]{spawn,superman,greenLantern};
-        for (IFlightSpeed hero : flyingHeroes ) {
-            System.out.println(hero.flightSpeed("super fast"));
+        Iterator<Superhero> heroes = theLeague.iterator();
+        while(heroes.hasNext()){
+            Superhero result = heroes.next();
+            System.out.println(result );
         }
+
+
+        ArrayList<IFlightSpeed> flyers = new ArrayList<>();
+        flyers.add(greenLantern);
+        flyers.add(spawn);
+        flyers.add(superman);
+
+        Iterator<IFlightSpeed> flyingHeroes = flyers.iterator();
+        while(flyingHeroes.hasNext()){
+            IFlightSpeed result = flyingHeroes.next();
+            System.out.println(result + "\n" + result.flightSpeed("really high!"));
+        }
+//        Superhero[] pantheon = new Superhero[]{ nightcrawler, spawn, spiderman, theFlash, superman};
+//        for (Superhero hero : pantheon){
+//            System.out.println(hero.listPowers());
+//        }
+//        System.out.println( spawn.myBlackHero() + " and " + spawn.flightSpeed("9999 mph"));
+//        System.out.println( superman.flightSpeed("super fast"));
+//
+//        IFlightSpeed[] flyingHeroes = new IFlightSpeed[]{spawn,superman,greenLantern};
+//        for (IFlightSpeed hero : flyingHeroes ) {
+//            System.out.println(hero.flightSpeed("super fast"));
+//        }
 
     }
 }
